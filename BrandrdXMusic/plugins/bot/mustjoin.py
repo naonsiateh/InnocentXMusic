2a021a15d585b3852b8bc1c84ca7d9604fda8dcc
naonsiateh <ekayulianti.yantii@gmail.com>
2025-04-08 12:08:29 +0700

    Update mustjoin.py

--- BrandrdXMusic/plugins/bot/mustjoin.py
+++ BrandrdXMusic/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from BrandrdXMusic import app
 
 #--------------------------
 
-MUST_JOIN = "https://t.me/DilanSupport"
+MUST_JOIN = "DilanSupport"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
